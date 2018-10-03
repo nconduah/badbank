d89e81c355e9523aef1d672b1647633f59ec923a
Nana EC <nconduah@gmail.com>
2018-10-03 19:16:08 -0400

    add nbb image and change port for public run

--- public/modules/modules.js
+++ public/modules/modules.js
@@ -2,7 +2,7 @@ var ui = {};
 
 ui.navigation = `
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="#" onclick="defaultModule()">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
         
     </a>
@@ -191,11 +191,11 @@ ui.balance = `
 
 ui.default = `
 <div class="card bg-light mb-3" style="max-width: 18rem;">
-    <div class="card-header"><h4>NAess BadBank Landing Module<h2/></div>
+    <div class="card-header"><h4>NAess BadBank<h2/></div>
     <div class="card-body">
         <h5 class="card-title">Welcome to the bank</h5>
         <br/>
-        <p class="card-text">You can move around using the navigation bar.</p>
+        <p class="card-text">Authetication isn't our strong suit but we're at least a 1.0 version of a bank :). You can move around using the navigation bar.</p>
         <br/>
         <img class="card-img-bottom" src="nbb.png" alt="NAess BadBank cap">
     </div>
